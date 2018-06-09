e554987b1f3395a7896971bbd14533f7f670575a
AYAKO SAYAMA <>
2018-06-09 01:42:05 -0700

    6-1. ボタンをクリックできるようにする

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,11 @@ import { NavLink } from 'react-router-dom';
 import './Header.scss';
 
 export default class Header extends Component {
+
+  handleClick = () => {
+    console.log("Clicked!!");
+  }
+
   render() {
     return (
       <div className="Header">
@@ -11,37 +16,40 @@ export default class Header extends Component {
           <div className="Header-logo">Ayako Sayama</div>
           <Media query="(min-width: 540px)">
             {(matches) => (
-              matches ?
-              <div className="Header-nav">
-                <NavLink
-                  exact
-                  to="/"
-                  className="Header-nav-item"
-                  activeClassName="Header-nav-item--active"
-                >
-                  My Works
-                </NavLink>
-                <NavLink
-                  to="/about"
-                  className="Header-nav-item"
-                  activeClassName="Header-nav-item--active"
-                >
-                  About Me
-                </NavLink>
-                <NavLink
-                  to="/contact"
-                  className="Header-nav-item"
-                  activeClassName="Header-nav-item--active"
-                >
-                  Contact
-                </NavLink>
-              </div>
-              : 
-              <img
-                width={20}
-                alt="Menu Icon"
-                src={require("./../../assets/icons/menuIcon.svg")}
-              />
+              matches ? (
+                <div className="Header-nav">
+                  <NavLink
+                    exact
+                    to="/"
+                    className="Header-nav-item"
+                    activeClassName="Header-nav-item--active"
+                  >
+                    My Works
+                  </NavLink>
+                  <NavLink
+                    to="/about"
+                    className="Header-nav-item"
+                    activeClassName="Header-nav-item--active"
+                  >
+                    About Me
+                  </NavLink>
+                  <NavLink
+                    to="/contact"
+                    className="Header-nav-item"
+                    activeClassName="Header-nav-item--active"
+                  >
+                    Contact
+                  </NavLink>
+                </div>
+              ) : (
+                <button onClick={this.handleClick}>
+                  <img
+                    width={20}
+                    alt="Menu Icon"
+                    src={require("./../../assets/icons/menuIcon.svg")}
+                  />
+                </button>
+              )
             )}
           </Media>
         </div>
